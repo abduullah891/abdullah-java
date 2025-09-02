9cc6298f2bac69da639157040ef15a0cd3a9bad8
abdullah <firec4309@gmail.com>
2025-09-02 14:52:17 +0700

    Add files via upload

---
+++ tugas_formloggindanmaster/src/tugas/formloggindanmaster/TugasFormloggindanmaster.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.formloggindanmaster;
+
+/**
+ *
+ * @author Software24
+ */
+public class TugasFormloggindanmaster {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
